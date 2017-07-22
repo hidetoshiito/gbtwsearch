1190fc3ee6c61bbe5dfe27e0a5c2b79de349631c
hidetoshiito <hi0258@hotmail.com>
2017-07-22 14:16:04 +0900

    0.5.1

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,11 +29,240 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_twsearch = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "検索文";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_twsearch
+            // 
+            this.txt_twsearch.Location = new System.Drawing.Point(47, 6);
+            this.txt_twsearch.Name = "txt_twsearch";
+            this.txt_twsearch.Size = new System.Drawing.Size(190, 19);
+            this.txt_twsearch.TabIndex = 1;
+            this.txt_twsearch.Text = "Lv100 バアル";
+            this.txt_twsearch.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Icon";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(10, 99);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(227, 68);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "検索文でTW検索して参戦IDがあったら、\r\nクリップボードに勝手にコピーするアプリです。";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(10, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(227, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(243, 39);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(38, 19);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(243, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "★";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(292, 247);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txt_twsearch);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.numericUpDown1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(284, 221);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "グラブルTwitter検索";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(284, 221);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "初期設定";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(11, 16);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Twitter認証URLを取得";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(11, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(83, 19);
+            this.textBox2.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(11, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(154, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "PINを登録";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(11, 99);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(267, 110);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "twitter認証ページからこのアプリを許可してください";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(296, 251);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "グラブルの救援依頼を取得するアプリ";
+            this.Load += new System.EventHandler(this.Form1_LoadAsync);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_twsearch;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,9 +12,140 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        Boolean flag = true;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private  void Form1_LoadAsync(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            txt_twsearch.ReadOnly = true;
+            button1.Enabled = false;
+            var keyword = txt_twsearch.Text;
+            string battleid = "00000000";
+            int cnt = 0;
+            String[] star = {"★","｜","☆", "｜" };
+            flag = true;
+
+            try
+            {
+                var tokens = CoreTweet.Tokens.Create("9jz44UHH1Il3Pir09ihkl3Oc7", "04vcG5ZmG9ZCMaFH1wnfQ6tiUzIRVb6dAJBflYVooUr1Yy6mFu", Properties.Settings.Default.usertoken, Properties.Settings.Default.usertokenS);
+                while (flag)
+                {
+                    cnt++;
+                    var result = await tokens.Search.TweetsAsync(count => 1, q => keyword);
+                    Console.WriteLine(result.Count);
+                    foreach (var tweet in result)
+                    {
+                        Console.WriteLine("{0}: {1}",tweet.CreatedAt, tweet.Text);
+                        string[] a = tweet.Text.Split('\n');
+                        int s = a[0].IndexOf("参戦ID：");
+                        string nowbattleid = a[0].Substring(s + 5);
+                        if (battleid.Equals(nowbattleid))
+                        {
+                            break;
+                        }
+                        battleid = nowbattleid;
+                        var jst = tweet.CreatedAt.LocalDateTime;
+
+                        Clipboard.SetText(battleid);
+
+                        textBox1.Text = jst + "\r\n" + tweet.User.ScreenName + "\r\n" + tweet.Text.Replace("\n", "\r\n").Replace("\r\r", "\r");
+
+                        notifyIcon1.BalloonTipTitle = battleid;
+                        notifyIcon1.BalloonTipText = tweet.Text;
+                        notifyIcon1.ShowBalloonTip(1000);
+
+                    }
+                    await Task.Delay(Decimal.ToInt32(numericUpDown1.Value) * 250);
+                    label3.Text = star[((cnt * 4 + 0) % 4)] + ":" + cnt;
+                    await Task.Delay(Decimal.ToInt32(numericUpDown1.Value) * 250);
+                    label3.Text = star[((cnt * 4 + 1) % 4)] + ":" + cnt;
+                    await Task.Delay(Decimal.ToInt32(numericUpDown1.Value) * 250);
+                    label3.Text = star[((cnt * 4 + 2) % 4)] + ":" + cnt;
+                    await Task.Delay(Decimal.ToInt32(numericUpDown1.Value) * 250);
+                    label3.Text = star[((cnt * 4 + 3) % 4)] + ":" + cnt;
+                }
+            }
+            catch (CoreTweet.TwitterException er)
+            {
+                textBox1.Text = er.Message;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            flag = false;
+            txt_twsearch.ReadOnly = false;
+            button1.Enabled = true;
+        }
+
+
+        CoreTweet.OAuth.OAuthSession session;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                session = CoreTweet.OAuth.Authorize("9jz44UHH1Il3Pir09ihkl3Oc7", "04vcG5ZmG9ZCMaFH1wnfQ6tiUzIRVb6dAJBflYVooUr1Yy6mFu");
+                var url = session.AuthorizeUri; // -> user open in browser
+                textBox3.Text = "認証URLをクリップボードにコピーしました\r\n" +
+                                "ブラウザのURLバーにctrl+vで貼り付け\r\n" +
+                                "Twitter認証を行いPINコードを取得→入力→登録してちょ。\r\n" +
+                                url.AbsoluteUri;
+                Clipboard.SetText(url.AbsoluteUri);
+            }
+            catch (CoreTweet.TwitterException er)
+            {
+                textBox3.Text = "失敗！\r\n" + er.Message;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var tokens = CoreTweet.OAuth.GetTokens(session, textBox2.Text);
+                Properties.Settings.Default.usertoken = tokens.AccessToken;
+                Properties.Settings.Default.usertokenS = tokens.AccessTokenSecret;
+                Properties.Settings.Default.Save();
+                textBox3.Text = "認証完了!";
+            }
+            catch (CoreTweet.TwitterException er)
+            {
+                textBox3.Text = "失敗！\r\n" + er.Message;
+            }
+            catch (System.Exception ex)
+            {
+                textBox3.Text = "エラー！\r\n" + ex.Message;
+            }
+
         }
     }
 }
